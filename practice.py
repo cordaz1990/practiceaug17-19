9a6e1b5e45b0cfac1c3343ebd240eb7ac651966b
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 17:53:26 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,13 @@
-print('hello')
-print('world')
-print('world')
+def printall(*args):
+    print(args)
+    
+>>> printall(1, 2.0, '3')
+(1, 2.0, '3')
+
+>>> t = (7, 3)
+>>> divmood(t)
+>>> divmod(*t)
+(2, 1)
+
+>>>max(1,2,3)
+>>>sum(1,2,3)
